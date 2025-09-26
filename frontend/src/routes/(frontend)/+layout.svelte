1be7139d41334733cdd6f385c600ae1517a95c80
Albin <albin@conicsolutions.app>
2025-09-26 04:33:02 +0200

    Navigation added

--- frontend/src/routes/(frontend)/+layout.svelte
+++ frontend/src/routes/(frontend)/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import '$lib/styles/app.css';
+  import Navigation from '$lib/components/frontend/common/Navigation.svelte';
 </script>
 
 <svelte:head>
   <link rel="icon" href="/favicon.svg" />
 </svelte:head>
+
+<Navigation />
 
 <slot />
